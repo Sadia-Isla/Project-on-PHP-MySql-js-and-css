5f2fd2eab7d3d9955b6c3f1a35c573e729a61ed9
Sadia Islam <83303610+Sadia-Isla@users.noreply.github.com>
2021-04-27 20:40:47 -0400

    file

---
+++ ProductFiles/includes/footer.php
@@ -0,0 +1,132 @@
+ <!-- Footer  -->
+	 
+	 <!-- Stylesheet -->
+        <link href="css/footer.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">		
+        
+		<!-- Category Start-->
+        <div class="category">
+            <div class="container-fluid">
+               <div class="row">
+                    <div class="col-md-3">
+                        <div class="category-item ch-400">
+                            <img src="brandsimage/category-1.jpg" />
+                            <a class="category-name" href="">
+                                <p>Fashion</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="category-item ch-250">
+                            <img src="brandsimage/toys.jpg" />
+                            <a class="category-name" href="">
+                                <p>Best Toys</p>
+                            </a>
+                        </div>
+                        <div class="category-item ch-150">
+                            <img src="brandsimage/makeup-1.jpg" />
+                            <a class="category-name" href="">
+                                <p>Makeup</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="category-item ch-150">
+                            <img src="brandsimage/makeup.jpg" />
+                            <a class="category-name" href="">
+                                <p>Discover Yourself</p>
+                            </a>
+                        </div>
+                        <div class="category-item ch-250">
+                            <img src="brandsimage/gadgets.jpg" />
+                            <a class="category-name" href="">
+                                <p> Buy Essentials</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="category-item ch-400">
+                            <img src="brandsimage/product 8.jpg" />
+                            <a class="category-name" href="">
+                                <p> Life Styles</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Category End-->       
+        
+		
+		 <!-- Footer --> 
+		<div class="footer">
+            <div class="container-fluid">
+               <div class="row"> 
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Get in Touch</h2>
+                            <div class="contact-info">
+                                <p><i class="fa fa-map-marker"></i>123 Looka, Ottawa, Canada</p>
+                                <p><i class="fa fa-envelope"></i>looka@gmail.com</p>
+                                <p><i class="fa fa-phone"></i>+1613-456-7890</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Follow Us</h2>
+                            <div class="contact-info">
+                                <div class="social">
+                                    <a href=""><i class="fab fa-twitter"></i></a>
+                                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                    <a href=""><i class="fab fa-instagram"></i></a>
+                                    <a href=""><i class="fab fa-youtube"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Company Info</h2>
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Terms & Condition</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Purchase Info</h2>
+                            <ul>
+                                <li><a href="#">Payment Policy</a></li>
+                                <li><a href="#">Shipping Policy</a></li>
+                                <li><a href="#">Return Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row payment align-items-center">
+                    <div class="col-md-6">
+                        <div class="payment-method">
+                            <h2>We Accept:</h2>
+                            <img src="brandsimage/payment-method.png" alt="Payment Method" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="payment-security">
+                            <h2>Secured By:</h2>
+                            <img src="brandsimage/ks.jpg" alt="Payment Security" />
+                            <img src="brandsimage/AVG.jpg" alt="Payment Security" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+    
